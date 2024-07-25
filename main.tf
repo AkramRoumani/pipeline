9ccc04ae46cb7d7a2595801cf635233a2ae5bbe8
Akram Roumani <124212528+AkramRoumani@users.noreply.github.com>
2024-07-25 14:19:31 +0100

    gg

--- main.tf
+++ main.tf
@@ -7,21 +7,22 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "example" {
+resource "azurerm_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"  # Utilisez un plan de niveau Basic pour éviter les limitations du plan gratuit
+    size = "B1"
   }
+  os_type = "Windows"
 }
 
 resource "azurerm_windows_web_app" "example" {
   name                = "example-appservice-unique-1234" # Changez ce nom pour qu'il soit unique
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  service_plan_id     = azurerm_service_plan.example.id
 
   site_config {
     default_documents = ["index.html"]

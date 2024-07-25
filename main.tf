f5bb744eab38b0ecfd0a71c09f720244500fdbaa
Akram Roumani <124212528+AkramRoumani@users.noreply.github.com>
2024-07-25 13:28:26 +0100

    web

--- main.tf
+++ main.tf
@@ -6,3 +6,24 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-appservice"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    app_command_line = "echo 'Hello World' > /home/site/wwwroot/index.html"
+  }
+}

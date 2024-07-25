89b6e9fe9b321c3b3a5e39b44c6d829a8011cf1b
Akram Roumani <124212528+AkramRoumani@users.noreply.github.com>
2024-07-25 13:35:12 +0100

    kk

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "example" {
+resource "azurerm_app_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -21,7 +21,7 @@ resource "azurerm_windows_web_app" "example" {
   name                = "example-appservice-unique-1234" # Changez ce nom pour qu'il soit unique
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  service_plan_id     = azurerm_service_plan.example.id
+  service_plan_id     = azurerm_app_service_plan.example.id
 
   site_config {
     app_command_line = "echo 'Hello World' > /home/site/wwwroot/index.html"
